1500c5f32b806f3762d4ff41ec2efb8004bf3abc
JanShemora <jan.semora@email.cz>
2023-03-15 09:57:08 +0100

    edit: spaces, tabs and enters

--- src/math/Arithmetic.java
+++ src/math/Arithmetic.java
@@ -2,36 +2,28 @@ package math;
 
 public class Arithmetic
 {
-	/**
-	 * sčítání čísel 'a' a 'b')
-	 */
+	/** sčítání čísel 'a' a 'b') */
 	public static double add(double a, double b)
 	{
 		double result = a + b;
 		return result;
 	}
 
-	/**
- 	 * od čísla 'a' odečte číslo 'b'
-	 */
+	/** od čísla 'a' odečte číslo 'b' */
 	public static double sub(double a, double b)
 	{
 		double result = a - b;
 		return result;
 	}
 
-	/**
-	 * násobí číslo 'a', číslem 'b'
-	 */
+	/** násobí číslo 'a', číslem 'b' */
 	public static double mul(double a, double b)
 	{
 		double result = a * b;
 		return result;
 	}
 
-	/**
-	 * dělí číslo 'a', číslem 'b'
-	 */
+	/** dělí číslo 'a', číslem 'b' */
 	public static double div(double a, double b)
 	{
 		if (b == 0){
@@ -43,18 +35,14 @@ public class Arithmetic
 		}
 	}
 
-	/**
-	 * vrací druhou mocninu čísla 'a'
-	 */
+	/** vrací druhou mocninu čísla 'a' */
 	public static double pwr(double a)
 	{
 		double result = a * a;
 		return result;
 	}
 
-	/**
-	 * vrací 'n'-tou mocninu čísla 'a'
-	 */
+	/** vrací 'n'-tou mocninu čísla 'a' */
 	public static double pwr(double a, int n)
 	{
 		if ( n < 0 )
@@ -73,23 +61,16 @@ public class Arithmetic
 			}
 			return result;
 		}
-		
 	}
-
-	/*
-	 * vrací 'n'-tou mocninu čísla 'a'
-	 * pro čísla s dessetinnou čárkou
-	 */
+	
+	/** vrací 'n'-tou mocninu čísla 'a' */
 	public static double pwr(double a, double b)
 	{
 		double result = Math.pow(a,b);
 		return result;
 	}
 
-
-	/**
-	 * vrací faktoriál čísla 'a'
-	 */
+	/** vrací faktoriál čísla 'a' */
 	public static double fac(int a)
 	{
 		if ( a < 0)
@@ -104,7 +85,6 @@ public class Arithmetic
 		{
 			return 1;
 		}
-
 		double result = a;
 		for (int i = a-1; i > 1; i--)
 		{
@@ -113,9 +93,7 @@ public class Arithmetic
 		return result;
 	}
 
-	/**
-	 * vrací druhou odmocninu čísla'a'
-	 */
+	/** vrací druhou odmocninu čísla'a' */
 	public static double sqrt(double a)
 	{
 		if ( a < 0 )
@@ -126,10 +104,7 @@ public class Arithmetic
     	return result;
 	}
 
-	/*
-	 * vrací n-tou odmocninu čísla 'a'
-	 * k tomu využívá stávající funkci sqrt() pro druhou odmocninu
-	 */
+	/* vrací n-tou odmocninu čísla 'a' */
 	public static double sqrt(double a, int b)
 	{
 		if ( a < 0 )
@@ -149,9 +124,7 @@ public class Arithmetic
 		return result;
 	}
 	
-	/*
-	 * vrací negovanou hodnotu čísla 'a'
-	 */
+	/* vrací negovanou hodnotu čísla 'a' */
 	public static double neg(double a)
 	{
 		double result = 0 - a;
