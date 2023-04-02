ade8039c3c17c34e4fdb747862bbb806a6bd0c10
tomas-sitarcik <t.sith96@gmail.com>
2023-04-02 22:07:14 +0200

    Added clear button and bound the virtual enter key to the Enter key on the keyboard.

--- src/calculator/Controller.java
+++ src/calculator/Controller.java
@@ -11,6 +11,9 @@ import javafx.fxml.Initializable;
 import javax.swing.Action;
 import javafx.event.ActionEvent;
 
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
 import math.Parser;
 
 public class Controller implements Initializable {
@@ -28,14 +31,17 @@ public class Controller implements Initializable {
 
     }
 
-    @FXML
-    private void submitPressed(ActionEvent event) {
+    private void submit() {
         try {
             output.setText(Double.toString(parser.parse(input.getText())));
         } catch (Exception e) {
             output.setText(e.getMessage());
         }
+    }
 
+    @FXML
+    private void submitPressed(ActionEvent event) {
+        submit();
     }
 
     @FXML
@@ -45,15 +51,36 @@ public class Controller implements Initializable {
         input.setText(input.getText() + button.getText());
     } 
 
-    @FXML
-    private void backspacePressed(ActionEvent event) {
-
+    private void backspace() {
         String inputText = input.getText();
 
         if (inputText.length() > 0) {
             input.setText(inputText.substring(0, inputText.length() - 1));
         }
-
     }
 
+    @FXML
+    private void backspacePressed(ActionEvent event) {
+        backspace();
+    }
+
+    @FXML
+    private void clear(ActionEvent event) {
+        input.setText("");
+    }
+
+    @FXML
+    private void onKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            submit();
+        }
+
+        /* 
+        // nefunguje správně - mažou se dva znaky
+        if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+            backspace();
+        }
+        */
+        //keyEvent.consume();
+    }
 }
